03b71d1fd94a77bfa487a494bf6184ead849a5bf
tmp <w@w>
2018-11-17 11:25:39 +0500

    Remove Drunkard class Refactor DrunkardList class

--- src/main/java/drunkard/DrunkardList.java
+++ src/main/java/drunkard/DrunkardList.java
@@ -5,10 +5,16 @@ import java.util.Collections;
 
 public class DrunkardList {
 
+    private static final int COUNT_CARDS = 36;
+    private static final int HALF_COUNT_CARDS = 18;
+
     private static ArrayList<Integer> cardsIds = new ArrayList<Integer>();
     private static ArrayList<Integer> cardsIdPlayer1 = new ArrayList<Integer>();
     private static ArrayList<Integer> cardsIdPlayer2 = new ArrayList<Integer>();
-    private static int firstPlayerScore = 18, secondPlayerScore = 18;
+    private static Integer cardIdPlayer1;
+    private static Integer cardIdPlayer2;
+    private static int firstPlayerScore = 18;
+    private static int secondPlayerScore = 18;
     private static int round = 0;
 
     private enum Suit {
@@ -28,24 +34,28 @@ public class DrunkardList {
     }
 
     static {
-        for(int i = 0; i < 36; i++)
-            cardsIds.add(i);
+        for(int i = 0; i < COUNT_CARDS;)
+            cardsIds.add(i++);
 
         Collections.shuffle(cardsIds);
 
-        for (int i = 0; i < 18; i++)
-            cardsIdPlayer1.add(cardsIds.get(i));
+        for (int i = 0; i < HALF_COUNT_CARDS;)
+            cardsIdPlayer1.add(cardsIds.get(i++));
 
-        for (int i = 18; i < 36; i++)
-            cardsIdPlayer2.add(cardsIds.get(i));
+        for (int i = HALF_COUNT_CARDS; i < COUNT_CARDS;)
+            cardsIdPlayer2.add(cardsIds.get(i++));
 
     }
 
     public static void main(String... __) {
 
         while (firstPlayerScore != 0 && secondPlayerScore != 0) {
-            roundInfo();
-            roundWinner(cardsIdPlayer1.get(0), cardsIdPlayer2.get(0));
+
+            cardIdPlayer1 = cardsIdPlayer1.get(0);
+            cardIdPlayer2 = cardsIdPlayer2.get(0);
+
+            roundInfo(cardIdPlayer1, cardIdPlayer2);
+            roundWinner(cardIdPlayer1, cardIdPlayer2);
             round++;
         }
 
@@ -56,49 +66,61 @@ public class DrunkardList {
         return Value.get(cardId) + " " + Suit.get(cardId);
     }
 
-    private static void roundInfo(){
+    private static void roundInfo(int cardIdPlayer1, int cardIdPlayer2){
         System.out.println();
         System.out.println("Round " + round);
-        System.out.println("First player: " + toString(cardsIdPlayer1.get(0)));
-        System.out.println("Second player: " + toString(cardsIdPlayer2.get(0)));
+        System.out.println("First player: " + toString(cardIdPlayer1));
+        System.out.println("Second player: " + toString(cardIdPlayer2));
     }
 
     private static void whoIsWinner(int firstPlayerScore){
         if (firstPlayerScore == 0) {
-            System.out.println("Second player in this game!!!");
+            System.out.println();
+            System.out.println("Second player win in this game!!!");
         } else {
-            System.out.println("First player in this game!!!");
+            System.out.println();
+            System.out.println("First player win in this game!!!");
         }
     }
 
     private static void roundWinner(int player1, int player2){
-        if (player1 % 9 == player2 % 9)
+        int value1 = player1 % 9;
+        int value2 = player2 % 9;
+        if (value1 == value2)
+            dispute();
+        else if (value1 == 0 && value2 == 8)
             firstWinRound();
-        else if (player1 % 9 == 0 && player2 % 9 == 8)
-            firstWinRound();
-        else if (player1 % 9 == 8 && player2 % 9 == 0)
+        else if (value1 == 8 && value2 == 0)
             secondWinRound();
-        else if (player1 % 9 < player2 % 9)
+        else if (value1 < value2)
             secondWinRound();
         else firstWinRound();
     }
 
-    public static void firstWinRound(){
+    private static void firstWinRound(){
         firstPlayerScore++;
         secondPlayerScore--;
         System.out.println("First player win in round!");
-        cardsIdPlayer1.add(cardsIdPlayer1.get(0));
-        cardsIdPlayer1.add(cardsIdPlayer2.get(0));
+        cardsIdPlayer1.add(cardIdPlayer1);
+        cardsIdPlayer1.add(cardIdPlayer2);
         cardsIdPlayer1.remove(0);
         cardsIdPlayer2.remove(0);
     }
 
-    public static void secondWinRound(){
+    private static void secondWinRound(){
         firstPlayerScore--;
         secondPlayerScore++;
         System.out.println("Second player win in round!");
-        cardsIdPlayer2.add(cardsIdPlayer2.get(0));
-        cardsIdPlayer2.add(cardsIdPlayer1.get(0));
+        cardsIdPlayer2.add(cardIdPlayer2);
+        cardsIdPlayer2.add(cardIdPlayer1);
+        cardsIdPlayer1.remove(0);
+        cardsIdPlayer2.remove(0);
+    }
+
+    private static void dispute(){
+        System.out.println("Duspute players!");
+        cardsIdPlayer2.add(cardIdPlayer2);
+        cardsIdPlayer1.add(cardIdPlayer1);
         cardsIdPlayer1.remove(0);
         cardsIdPlayer2.remove(0);
     }
